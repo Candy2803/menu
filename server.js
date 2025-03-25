3e202c1b65021462935ff6de069a31d4d7afae01
+ <+>
2025-03-25 10:52:26 +0300

    edited server.js

--- server.js
+++ server.js
@@ -5,14 +5,11 @@ import { fileURLToPath } from 'url';
 const app = express();
 const PORT = 3000;
 
-// Resolve __dirname in ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Serve static files from the React app build directory
 app.use(express.static(path.join(__dirname, 'build')));
 
-// For any route, serve the index.html from the build folder
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
